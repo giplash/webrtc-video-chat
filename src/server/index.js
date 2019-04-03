fc383433e2fdd9dc9b8617279f4c3e2eb5f11e1f
giplash <anashkinviktar@gmail.com>
2019-04-03 05:45:48 +0300

    initial commit

---
+++ src/server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import os from 'os';
+
+const app = express();
+
+app.use(express.static('dist'));
+
+app.get('/api/getUsername',
+    (req, res) => res.send({ username: os.userInfo().username })
+);
+
+app.listen(
+    process.env.PORT || 8080,
+    () => console.log(`Listening on port ${process.env.PORT || 8080}!`)
+);
